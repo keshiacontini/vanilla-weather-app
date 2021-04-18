af90935204308a5e06d447183cf6109a76f591f6
keshiacontini <82321034+keshiacontini@users.noreply.github.com>
2021-04-18 23:50:54 +1000

    Date and Time Working

--- src/app.js
+++ src/app.js
@@ -1,4 +1,43 @@
 
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let number = date.getDate();
+    let days = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+            ];
+   let day = days[date.getDay()];
+
+   let months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+            ];
+   let month = months[date.getMonth()];   
+
+
+
+   return `${day} ${number} ${month}, ${hours}:${minutes}`;
+
+}
+
+
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -6,15 +45,18 @@ function displayTemperature(response) {
     let descriptionElement = document.querySelector("#currentCondition");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt *1000);
 }
 
 let apiKey = "7c9392b6da6881615112ab3cb5f8a411";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
