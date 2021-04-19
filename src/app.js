b8273bd5c160ef952671cd450a90049449230c31
keshiacontini <82321034+keshiacontini@users.noreply.github.com>
2021-04-19 21:51:52 +1000

    Configured Celcius/Fahrenheit conversion

--- src/app.js
+++ src/app.js
@@ -48,8 +48,10 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
+    celciusTemperature = response.data.main.temp;
 
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+    temperatureElement.innerHTML = Math.round(celciusTemperature);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
@@ -65,13 +67,39 @@ function search(city) {
     axios.get(apiUrl).then(displayTemperature);
 }
 
-
 function handleSubmit(event){
     event.preventDefault()
     let cityInputElement = document.querySelector("#city-input")
     search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+    //remove active class
+    celcius.classList.remove("active");
+    fahrenheit.classList.add("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelciusTemperature(event) {
+    event.preventDefault();
+    celcius.classList.add("active");
+    fahrenheit.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celciusTemperature);
+}
+
+let celciusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit); 
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", displayFahrenheitTemperature)
+
+let celcius = document.querySelector("#celcius");
+celcius.addEventListener("click", displayCelciusTemperature)
+
+search("Melbourne");
