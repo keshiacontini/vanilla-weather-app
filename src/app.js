35a2b6fb30143b1db51c91a9a6d544ab33ba21c2
keshiacontini <82321034+keshiacontini@users.noreply.github.com>
2021-04-19 20:35:51 +1000

    Configured Weather Icon API

--- src/app.js
+++ src/app.js
@@ -39,13 +39,14 @@ function formatDate(timestamp) {
 
 
 function displayTemperature(response) {
-    console.log(response.data);
+    //console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#currentCondition");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
 
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -54,9 +55,11 @@ function displayTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt *1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
 }
 
+let city = "Melbourne"
 let apiKey = "7c9392b6da6881615112ab3cb5f8a411";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
